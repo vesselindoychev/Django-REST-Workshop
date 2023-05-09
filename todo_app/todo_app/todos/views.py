d78383543c960896960c14087c2e49f69d6d421c
Veselin Doychev <veselindoi123@gmail.com>
2023-05-09 13:59:38 +0300

    Made a project set up

--- todo_app/todo_app/todos/views.py
+++ todo_app/todo_app/todos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django import views as api_views
+from rest_framework import views as api_views
+from rest_framework.response import Response
 
-# Create your views here.
